7f7e7eeca9e4aee31b71c440b1b926d05fb4e504
dreamerlu <836226042@qq.com>
2017-08-09 18:05:26 +0800

    This is some codes when I learned the python

--- analyseData.py
+++ analyseData.py
@@ -3,20 +3,68 @@ import operator
 from operator import itemgetter
 from collections import OrderedDict
 import matplotlib.pyplot as plt
+plt.rc('font', family='SimHei', size=8)
+import numpy as np
 con=sqlite3.connect('J:/test.db')
 cursor=con.cursor()
-''' 分析不同区的商品数量 '''
+
 gamezone={11:'联通一区',12:'联通二区',13:'联通三区',14:'联通四区',15:'联通五区',16:'联通六区',
-          21:'电信一区',22:'电信二区',23:'电信三区',24:'电信四区',25:'电信五区',26:'电信六区',27:'电信七区',28:'电信八区'}
-resultDict=dict()
-for key in gamezone.keys():
-    result=cursor.execute('SELECT count(*) FROM t_yxdproduct WHERE gamezone=?',(gamezone[key],))
-    for row in result:
-        resultDict[key]=int(row[0])
-sorted_x = OrderedDict(sorted(resultDict.items(), key=itemgetter(1)))
-# 绘制柱状图
-plt.rc('font', family='SimHei', size=8)
-xaix=list(gamezone.values())
-yaix=list(resultDict.values())
-plt.bar(range(len(gamezone)),yaix,tick_label=xaix)
-plt.show()
+              21:'电信一区',22:'电信二区',23:'电信三区',24:'电信四区',25:'电信五区',26:'电信六区',27:'电信七区',28:'电信八区'}
+''' 分析不同区的商品数量 '''
+def getZoneProductNumber():
+
+    resultDict=dict()
+    for key in gamezone.keys():
+        result=cursor.execute('SELECT count(*) FROM t_yxdproduct WHERE gamezone=?',(gamezone[key],))
+        for row in result:
+            resultDict[key]=int(row[0])
+    sorted_x = OrderedDict(sorted(resultDict.items(), key=itemgetter(1)))
+    # 绘制柱状图
+    xaix=list(gamezone.values())
+    yaix=list(resultDict.values())
+    plt.bar(range(len(gamezone)),yaix,tick_label=xaix)
+    plt.show()
+
+''' 分析各个区的最高售价、最低售价、平均值 '''
+maxPrice,minPrice,avePrice=dict(),dict(),dict()
+def getZonePrice():
+    for key in gamezone.keys():
+        result = cursor.execute('SELECT max(price),min(price),avg(price) FROM t_yxdproduct WHERE gamezone=?', (gamezone[key],))
+        for row in result:
+            maxPrice[key]=row[0]
+            minPrice[key]=row[1]
+            avePrice[key]=round(row[2],2)
+    print(maxPrice,minPrice,avePrice)
+    #  绘制多柱状图
+    n_groups = len(gamezone.keys())
+    # means_men = (20, 35, 30, 35, 27)
+    # std_men = (2, 3, 4, 1, 2)
+    # means_women = (25, 32, 34, 20, 25)
+    # std_women = (3, 5, 2, 3, 3)
+    fig, ax = plt.subplots()
+    index = np.arange(n_groups)
+    bar_width = 0.35
+    opacity = 0.4
+    # error_config = {'ecolor': '0.3'}
+    rects1 = plt.bar(index, tuple(maxPrice.values()), bar_width,
+                     alpha=opacity,
+                     color='b',
+                     label='最高价')
+    rects2 = plt.bar(index + bar_width, tuple(avePrice.values()), bar_width,
+                     alpha=opacity,
+                     color='r',
+                     label='平均价')
+    rects3 = plt.bar(index + bar_width*2, tuple(minPrice.values()), bar_width,
+                     alpha=opacity,
+                     color='g',
+                     label='最低价')
+    plt.xlabel('游戏区')
+    plt.ylabel('价格')
+    plt.title('出售价格分析')
+    plt.xticks(index + 2*bar_width / 2, tuple(gamezone.values()))
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+''' 测试区 '''
+getZonePrice()
